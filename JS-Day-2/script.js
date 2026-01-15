3f8133763c3613d7203fa868d8639f38a4be6c29
rohitrksingh1920 <rohitrk.singh1920@gmail.com>
2026-01-15 18:37:37 +0530

    JS Updated

--- JS-Day-2/script.js
+++ JS-Day-2/script.js
@@ -9,9 +9,9 @@
 
 // reduce method
 
-let numbers = [1, 2, 3, 4, 5];
-let sum = numbers.reduce((acc, current) => acc + current);
-console.log(sum)
+// let numbers = [1, 2, 3, 4, 5];
+// let sum = numbers.reduce((acc, current) => acc + current);
+// console.log(sum)
 // acc=1, index = 0
 // current: rest of the elements apart from index:0
 // let sum = 0;
@@ -20,3 +20,69 @@ console.log(sum)
 //     sum += numbers[i];
 // }
 // console.log(sum)
+
+
+// Q 1. Iterate all the element and print the elements
+
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// for( let i = 0; i <= arr.length-1; i++) {
+//     console.log(arr[i]);
+// }
+
+// Q 2. Add 5 to each number inside the array and take array length 5.
+
+// let arr = [10, 20, 30, 40, 50];
+
+// for(let i = 0; i <= arr.length-1; i++) {
+//     console.log(arr[i]+5);
+// }
+// console.log("array length: ",arr.length);
+
+// Q 3. reverse the array. The elements should be numeric
+
+// let arr = [5, 7, 9, 10, 12];
+
+// for(let i = arr.length-1; i >=0; i--) {
+//     console.log(arr[i]);
+// }
+
+// Q 4. sort the array in desending order
+
+// let arr = [5, 2, 9, 1, 7];
+
+// for (let i = 0; i <= arr.length-1; i++) {
+//     for (let j = i + 1; j <= arr.length-1; j++) {
+//         if (arr[i] < arr[j]) {
+
+//             let temp = arr[i];
+//             arr[i] = arr[j];
+//             arr[j] = temp;
+//         }
+//     }
+// }
+// console.log(arr);
+
+// Q 5. Find the duplicate element and return the unique array
+
+let arr = [1, 2, 3, 2, 4, 1, 5, 8, 5];
+
+let unique = [];
+
+for (let i = 0; i <= arr.length-1; i++) {
+    let isDuplicate = false;
+
+    for (let j = 0; j <= unique.length-1; j++) {
+        if (arr[i] === unique[j]) {
+            isDuplicate = true;
+            console.log("duplicated:", arr[i]);
+            break;
+        }
+    }
+
+    if (!isDuplicate) {
+        unique.push(arr[i]);
+    }
+}
+
+console.log("Unique Array:", unique);
