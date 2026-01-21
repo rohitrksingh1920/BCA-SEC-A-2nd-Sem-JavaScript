402f1e721d7475de2dbe86124ed9363625fa3bc7
rohitrksingh1920 <rohitrk.singh1920@gmail.com>
2026-01-21 10:39:31 +0530

    day5

---
+++ JS-Day-4/script.js
@@ -0,0 +1,80 @@
+// let numbers = [1, 2, 3, 4, 5,];
+// let sliced = numbers.slice(1, 4);
+// console.log(sliced);
+
+
+
+// let num = [1, 3, 4, 5, 9];
+// let hasEven = num.some(x => x % 2 === 0);
+// console.log(hasEven);
+
+
+
+// let number = [2, 4, 6, 8, 10];
+// let allEven = num.every(x => x % 2 === 0);
+// console.log(allEven);
+
+// let fruits = ['apple', 'banana', 'cherry'];
+// fruits.splice(0, 2, 'orange', 'grape');
+
+// console.log(fruits);
+
+
+
+
+
+
+// index of element
+
+// let fruits = ['apple', 'mango', 'orange'];
+// let index = fruits.indexOf('orange');
+
+// console.log(index);
+
+
+// Slice(start, end)
+
+// let arr = [1, 2, 3, 4, 5];
+// let subArr = arr.slice(1, 4);
+
+// console.log(subArr);
+
+
+// some(callback)
+
+// let arr = [1, 2, 4, 3, 5, 7];
+// let hasEven = arr.some(x => x%2 === 0);
+// console.log(hasEven);
+
+// let arr = [1, 3, 5, 7, 9, 11];
+// let hasEven = arr.some(x => x%2 === 0);
+
+// console.log(hasEven);
+
+
+// every(callback)
+
+// let arr = [2, 4, 6, 8, 10, 12];
+
+// let allEven = arr.every(x => x%2 === 0);
+
+// console.log(allEven);
+
+
+// concat(array)
+
+// let arr1 = ['apple', 'orange', 'mango'];
+// let arr2 = ['cherry', 'grapes', 'banana'];
+
+// let con = arr1.concat(arr2);
+// console.log(con);
+
+
+// splice(start, deleteCount, item1, item2, ...)
+
+// let arr = [];
+
+
+
+// string method
+
---
+++ JS-Day-5/script.js
@@ -0,0 +1,77 @@
+// string methods
+// let str = "JavaScript is fun!";
+// let sliced = str.slice(0, 10);
+// console.log(sliced);
+
+// let str = "JavaScript is fun!";
+// let sub = str.substring(-2, 2);
+// console.log(sub); //negative index supports this but not slice
+
+// const paragraph = 'The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
+// const indexOfFirst = paragraph.indexOf("dog");
+// console.log(indexOfFirst);
+
+// let str = "OpenAI is innovative!";
+// let sub = str.substring(8, 2); // sub is "is"
+
+
+// let firstName = "John";
+// let lastName = "Doe";
+// let fullName = firstName.concat(" ", lastName);
+
+// console.log(fullName);
+
+// string literls
+
+// let fullName = `${firstName} : this is my firstname, ${lastName} : this is my lastname`
+
+// console.log(fullName)
+
+// let uni = 'K.R Mangalam';
+// let prog = 'BCA AI/DS';
+
+// console.log('My name is Rohit and im pursuing'+' '+ prog + ' from ' + uni);
+
+// let details = `My name is Rohit and im pursuing ${prog} form ${uni}`;
+
+// console.log(details);
+
+
+
+// function palindrome() {
+//     let arr = [1, 2, 3, 2, 1];
+//     let temp = [];
+//     let original = [...arr];
+
+    // for(let i = 0; i <= arr.length-1; i++) {
+    //     original.push(arr[i]);
+    // }
+    // console.log(original);
+
+//     while(arr.length > 0) {
+//         temp.push(arr.pop());
+//     }
+
+//     for(let i = 0; i <= original.length-1; i++) {
+//         if(original[i] === temp[i]) {
+//             return "palindrome";
+//         }
+//         else {
+//             return "not a palindrome";
+//         }
+//     }
+// }
+// console.log(palindrome());
+
+// let arr = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+// let new_array = [...arr, ...arr2];
+
+// console.log(new_array);
+
+
+// let sentence = "   This is a sentence with spaces.   ";
+// let trimmed = sentence.trim(); 
+
+// let sentence = "I,am,a,comma,separated,sentence";
+// let words = sentence.split(","); 
