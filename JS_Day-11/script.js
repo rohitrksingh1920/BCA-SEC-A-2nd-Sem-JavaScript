00a2d61eab25f166a6cc78a205f2afb3e91a8e07
rohitrksingh1920 <rohitrk.singh1920@gmail.com>
2026-02-08 14:39:24 +0530

    Used AddEventListner

--- JS_Day-11/script.js
+++ JS_Day-11/script.js
@@ -9,15 +9,22 @@
 //     document.getElementById("btn").textContent = "color changed"
 // }
 
-
 // Q2
 
-const handelClick = () => {
-    const image = document.createElement("img");
-    image.setAttribute("src", "https://krmangalam.edu.in/_next/image?url=%2Flife-at-krmu%2Finfra.webp&w=3840&q=75")
+// const handelClick = () => {
+//     const image = document.createElement("img");
+//     image.setAttribute("src", "https://krmangalam.edu.in/_next/image?url=%2Flife-at-krmu%2Finfra.webp&w=3840&q=75")
 
-    image.setAttribute("alt", "KR Mangalam");
+//     image.setAttribute("alt", "KR Mangalam");
 
-    document.getElementById("content").append(image);
+//     document.getElementById("content").append(image);
 
-}
+// }
+
+const button = document.getElementById("btn");
+let link = document.getElementById("link");
+let box = document.getElementById("box");
+button.addEventListener("click", function () {
+  link.style.display = "inline";
+  box.style.display = "flex";
+});
